2ade0735afc2dbcda303256a738ea304675e8fb5
Julian Fernandez <julianfernandez1811@gmail.com>
2024-05-12 14:51:00 -0300

    add chat component design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+    },
     extend: {},
   },
   plugins: [],
